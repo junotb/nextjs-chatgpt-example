58e65bae619940979045369e9ddd6eca42bd1d24
jhlee <jhlee@spicusinc.com>
2024-03-08 09:05:57 +0900

    edit service

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <>Hello</>
+    <div className="flex flex-col w-full min-h-screen justify-center items-center">
+      <div className="flex flex-col border border-[rgb(var(--color-rgb))] p-1 w-full sm:w-96 h-full rounded-md">
+        <header className="flex h-4">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 text-red-500">
+            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clipRule="evenodd" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 text-amber-500">
+            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm3 10.5a.75.75 0 0 0 0-1.5H9a.75.75 0 0 0 0 1.5h6Z" clipRule="evenodd" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 text-green-500">
+            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clipRule="evenodd" />
+          </svg>
+        </header>
+        <main className="">
+          Hello
+        </main>
+        <footer className="flex justify-between">
+          <div></div>
+          <button className="border border-[rgb(var(--color-rgb))] px-6 text-xs rounded-md">전송</button>
+        </footer>
+      </div>
+    </div>
   );
 }
